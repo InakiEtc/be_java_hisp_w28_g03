4b626625d13ebc897cfa91c022fbfceef01fc233
lucasbianchi0 <lucmbianchi2000@gmail.com>
2025-01-02 16:36:55 -0300

    fix: se limpia codigo

--- src/test/java/com/mercadolibre/socialmeli_g3/integration/controller/PostControllerTest.java
+++ src/test/java/com/mercadolibre/socialmeli_g3/integration/controller/PostControllerTest.java
@@ -138,7 +138,6 @@ public class PostControllerTest {
     @Test
     @DisplayName("T-0012 Find products on promo by user should return PromoProductPostListDTO")
     void test_findProductsOnPromoByUser_should_return_PromoProductPostListDTO() throws Exception {
-
         String userId = "1";
         PromoProductPostListDTO responseWaited = new PromoProductPostListDTO(1, "vendedor1",
                 Arrays.asList(
@@ -157,7 +156,7 @@ public class PostControllerTest {
 
         ResultMatcher expectedBody = content().json((mapper.writeValueAsString(responseWaited)));
         ResultMatcher expectedStatusCode = status().isOk();
-        ResultMatcher expectedContentType = content().contentType("application/json");
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(get("/products/promo-post/list")
                         .param("user_id", userId)
@@ -168,29 +167,15 @@ public class PostControllerTest {
                 .andDo(print());
     }
 
+
     @Test
-    @DisplayName("T-0012 Find products on promo by user should return PromoProductPostListDTO")
-    void test_findProductsOnPromoByUser_should_return_PromoProductPostListDTO() throws Exception {
+    @DisplayName("T-0012 Find products on promo by user should return NotFoundException")
+    void test_findProductsOnPromoByUser_should_throw_error() throws Exception {
+        String userId = "100";
 
-        String userId = "1";
-        PromoProductPostListDTO responseWaited = new PromoProductPostListDTO(1, "vendedor1",
-                Arrays.asList(
-                        new PostDTO(201, 1, "20-12-2024",
-                                new ProductDTO(101, "Silla Gamer", "Gamer", "Racer", "Red & Black", "Special Edition"),
-                                100, 1500.50, true, 0.40),
-
-                        new PostDTO(202, 1, "21-11-2024",
-                                new ProductDTO(102, "Teclado Mecánico", "Teclado", "Logitech", "Black", "RGB Backlit"),
-                                58, 250.00, true, 0.30),
-
-                        new PostDTO(203, 1, "03-08-2023",
-                                new ProductDTO(103, "Mouse Gamer", "Gamer", "Razer", "Green", "Wireless"),
-                                60, 120.00, true, 0.25)
-                ));
-
-        ResultMatcher expectedBody = content().json((mapper.writeValueAsString(responseWaited)));
-        ResultMatcher expectedStatusCode = status().isOk();
-        ResultMatcher expectedContentType = content().contentType("application/json");
+        ResultMatcher expectedBody = content().json(mapper.writeValueAsString(new ExceptionDTO("User not found")));
+        ResultMatcher expectedStatusCode = status().isNotFound();
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(get("/products/promo-post/list")
                         .param("user_id", userId)
